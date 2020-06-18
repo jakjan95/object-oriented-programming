4b09bd07ea874d60c3aade5cd944b6af6d2d0617
jakjan95 <jakubjanusz95@gmail.com>
2020-06-18 21:11:22 +0200

    lecture2

--- shm/cargo.hpp
+++ shm/cargo.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cmath>
 #include <string>
 
 class Cargo {
@@ -7,16 +8,76 @@ public:
     Cargo();
     Cargo(std::string name, size_t ammount, double basePrice);
 
-    std::string getName() const { return name_; };
-    size_t getAmmount() const { return ammount_; };
-    double getBasePrice() const { return basePrice_; };
+    virtual double getPrice() const = 0;
+    virtual std::string getName() const = 0;
+    virtual size_t getAmmount() const = 0;
+    virtual double getBasePrice() const = 0;
 
     Cargo& operator+=(const size_t& ammount);
     Cargo& operator-=(const size_t& ammount);
     bool operator==(const Cargo& freight2);
 
-private:
+protected:
     std::string name_;
     size_t ammount_;
     double basePrice_;
 };
+
+class Fruit : public Cargo {
+    size_t getRottenTime() const { return timeToRotten_; }
+    std::string getName() const override { return name_; };
+    double getPrice() const override {
+        return basePrice_ * (1 / std::exp(timeToRotten_));
+    }
+    double getBasePrice() const override { return basePrice_; }
+    virtual size_t getAmmount() const override { return ammount_; }
+
+    Fruit& operator--() {
+        if (timeToRotten_ > 0) {
+            timeToRotten_--;
+        }
+        return *this;
+    }
+
+private:
+    size_t timeToRotten_;
+};
+
+class Alcohol : public Cargo {
+    std::string getName() const override { return name_; };
+    double getPrice() const override {
+        if (volume_ == 0.96) {
+            return getBasePrice();
+        }
+        return getBasePrice() * (0.96 - volume_);
+    }
+
+    double getBasePrice() const override { return basePrice_; }
+    virtual size_t getAmmount() const override { return ammount_; }
+
+private:
+    const double volume_ = 0.96;
+};
+
+enum class Rarity {
+    common = 1,
+    rare = 2,
+    epic = 5,
+    legendary = 10,
+};
+
+class Item : public Cargo {
+    std::string getName() const override { return name_; };
+    double getPrice() const override { return getBasePrice() * static_cast<int>(rare); }
+
+    double getBasePrice() const override { return basePrice_; }
+    virtual size_t getAmmount() const override { return ammount_; }
+
+private:
+    Rarity rare;
+};
+
+int main() {
+    Fruit fruit;
+    return 0;
+}
--- shm/ship.hpp
+++ shm/ship.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <algorithm>
+#include <memory>
 #include <string>
 #include <vector>
 
@@ -16,12 +18,31 @@ public:
     size_t getSpeed() const { return speed_; }
     size_t getMaxCrew() const { return maxCrew_; }
     size_t getCapacity() const { return capacity_; }
-    std::vector<Cargo> getCargo() const { return cargo_; }
+    std::vector<std::shared_ptr<Cargo>> getCargo() const { return cargo_; }
 
     void setName(const std::string& name);
 
     Ship& operator+=(const size_t& crew);
     Ship& operator-=(const size_t& crew);
+
+    void load(std::shared_ptr<Cargo> cargo) {
+        //akumulacja tak zeby tego typu towary byly w jednym miejscu xD
+        //np nie 2x po 5 jablek a jablek 10
+        cargo_.push_back(cargo);
+    }
+
+    void unload(Cargo* cargo) {
+        //nazwy znalexc i tez czasy do zepsucia 
+        //zeby dobre znalezc
+        auto it = std::find(cargo_.begin(), cargo_.end(), cargo);
+        if (it != std::end(cargo_)) {
+            if (cargo->getAmmount() == 1) {
+                cargo_.erase(it);
+            } else {
+                it--;
+            }
+        }
+    }
 
 private:
     int id_;
@@ -30,5 +51,5 @@ private:
     size_t maxCrew_;
     size_t capacity_;
     size_t crew_;
-    std::vector<Cargo> cargo_;
+    std::vector<std::shared_ptr<Cargo>> cargo_;
 };
